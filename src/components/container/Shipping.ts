de0f8d823aff1775ac657d7b4d5ba908ec10bc1b
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2025-11-05 17:13:08 -0300

    Se refactoriza la función createHTML() de components/container/Shipping.ts haciendo que su contenido sea en base a appendchilds

--- src/components/container/Shipping.ts
+++ src/components/container/Shipping.ts
@@ -9,38 +9,58 @@ export class Shipping extends Container {
     }
 
     private createHTML (title: string, icon: string): void {
-        const html = `
-            <div class="row justify-content-center"> <!-- Centramos la fila -->
-                <div class="col-12 col-md-10"> <!-- Limitamos el ancho en pantallas grandes -->
-                    <div class="row mx-3 mx-sm-5 px-3 ">
-                        <div class="col-md-12">
-                            <h2 class="mb-4">
-                                ${createTitle(title, icon).outerHTML}
-                            </h2>
-                        </div>        
-                        <div class="col-12 col-md-4 my-3 my-md-0 px-3">
-                            ${this.createShippingOption(0)}
-                        </div>
-                        <div class="col-12 col-md-4 mb-3 my-md-0 px-3">
-                            ${this.createShippingOption(1)}
-                        </div>
-                        <div class="col-12 col-md-4 my-md-0 px-3">
-                            ${this.createShippingOption(2)}
-                        </div>
-                        <div class="col-12 col-md-8 p-3 shipping_rules_container rounded-3">
-                            ${this.createShippingOption(3)}
-                        </div>
-                    </div>
-                </div>
-            </div>`;        
-        this.setHTML(html);
+        const div1          = document.createElement('div');
+        div1.className      = 'row justify-content-center'; // Centramos la fila
+
+        const div2          = document.createElement('div');
+        div2.className      = 'col-12 col-md-10';           // Limitamos el ancho en pantallas grandes
+
+        const div3          = document.createElement('div');
+        div3.className      = 'row mx-3 mx-sm-5 px-3';
+
+        const divTitle      = document.createElement('div');
+        divTitle.className  = 'col-md-12';
+
+        const h2            = document.createElement('h2');
+        h2.className        = 'mb-4';
+
+        h2.appendChild(createTitle(title, icon));
+        
+        divTitle.appendChild(h2);
+        
+        const divOption0        = document.createElement('div');
+        divOption0.className    = 'col-12 col-md-4 my-3 my-md-0 px-3';
+        divOption0.innerHTML    = this.createShippingOption(0);
+
+        const divOption1        = document.createElement('div');
+        divOption1.className    = 'col-12 col-md-4 mb-3 my-md-0 px-3';
+        divOption1.innerHTML    = this.createShippingOption(1);
+
+        const divOption2        = document.createElement('div');
+        divOption2.className    = 'col-12 col-md-4 my-md-0 px-3';
+        divOption2.innerHTML    = this.createShippingOption(2);
+
+        const divOption3        = document.createElement('div');
+        divOption3.className    = 'col-12 col-md-8 p-3 shipping_rules_container rounded-3';
+        divOption3.innerHTML    = this.createShippingOption(3);
+
+        div3.appendChild(divTitle);
+        div3.appendChild(divOption0);
+        div3.appendChild(divOption1);
+        div3.appendChild(divOption2);
+        div3.appendChild(divOption3);
+
+        div2.appendChild(div3);
+        div1.appendChild(div2);
+
+        this.setHTML(div1.outerHTML);
     }
 
     private createShippingOption (i: number): string {
         if (arrShippingOptions[i]?.subtitle && arrShippingOptions[i]?.description) {
             return `
                 <h4 class="subtitles">${arrShippingOptions[i].subtitle}</h4>
-                ${arrShippingOptions[i].description.map(desc => `
+                ${arrShippingOptions[i].description.map((desc) => `
                     <p>${desc}</p>
                 `).join('')}`;
         }
