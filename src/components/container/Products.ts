5f4b4588ff81632b33493ed07ad7d24ad1f4158c
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2025-11-05 17:27:45 -0300

    Se refactoriza la función createCarouselImages() de components/container/Products.ts haciendo que su contenido sea en base a appendchilds

--- src/components/container/Products.ts
+++ src/components/container/Products.ts
@@ -109,11 +109,18 @@ export class Products extends Container{
         const carouselImg       = document.createElement('div');
         carouselImg.className   = 'carousel-inner';
         carouselImg.innerHTML   = card.carouselImages.map((_, k) => {
-            return `
-                <div class="carousel-item${k === 0 ? ' active' : ''}">
-                    <img src="${imagesPath}productsContainer/${product.id}/product${j + 1}_img${k + 1}.jpg" class="d-block w-100" alt="Imagen ${k + 1}" />
-                </div>`;
-            }).join('');
+            const div = document.createElement('div');
+            div.className = `carousel-item${k === 0 ? ' active' : ''}`;
+
+            const img = document.createElement('img');
+            img.src = `${imagesPath}productsContainer/${product.id}/product${j + 1}_img${k + 1}.jpg`;
+            img.className = 'd-block w-100';
+            img.alt = `Imagen ${k + 1}`;
+
+            div.appendChild(img);
+
+            return div.outerHTML;
+        }).join('');
         return carouselImg;
     }
 
