a6373afdb44bc09ceb73240b5700f1656d0c06ee
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2025-11-22 09:10:10 -0300

    Se adpta src/components/container/Hero.ts a la nueva clase Anchor.ts

--- dist/components/container/Hero.js
+++ dist/components/container/Hero.js
@@ -1,4 +1,5 @@
 import { Container } from "../Container.js";
+import { AnchorElement } from "../elements/Anchor.js";
 import { HeadingElement } from "../elements/Heading.js";
 import { ParagraphElement } from "../elements/Paragraph.js";
 export class Hero extends Container {
@@ -16,10 +17,15 @@ export class Hero extends Container {
             className: 'lead mb-4 fw-semibold',
             text: 'Somos distribuidores de una gran variedad de modelos de calzado infantil y calzado para adultos.'
         });
+        const a = new AnchorElement({
+            className: 'btn btn-primary btn-lg',
+            href: '#products-container',
+            text: 'Ver Productos'
+        });
         const html = `
             ${h1.getHeading().outerHTML}
             ${p.getParagraph().outerHTML}
-            <a href="#products-container" class="btn btn-primary btn-lg">Ver Productos</a>`;
+            ${a.getAnchor().outerHTML}`;
         this.setHTML(html);
     }
 }
--- src/components/container/Hero.ts
+++ src/components/container/Hero.ts
@@ -1,5 +1,6 @@
-import { Container } from "../Container.js";
-import { HeadingElement } from "../elements/Heading.js";
+import { Container }        from "../Container.js";
+import { AnchorElement }    from "../elements/Anchor.js";
+import { HeadingElement }   from "../elements/Heading.js";
 import { ParagraphElement } from "../elements/Paragraph.js";
 
 export class Hero extends Container {
@@ -10,20 +11,26 @@ export class Hero extends Container {
     
     private createHTML (): void {
         const h1 = new HeadingElement({
-            className: 'display-4 fw-bold mb-4',
-            text: 'Calzados para Toda la Familia',
-            type: 'h1'
+            className   : 'display-4 fw-bold mb-4',
+            text        : 'Calzados para Toda la Familia',
+            type        : 'h1'
         });
 
         const p = new ParagraphElement({
-            className: 'lead mb-4 fw-semibold',
-            text: 'Somos distribuidores de una gran variedad de modelos de calzado infantil y calzado para adultos.'
+            className   : 'lead mb-4 fw-semibold',
+            text        : 'Somos distribuidores de una gran variedad de modelos de calzado infantil y calzado para adultos.'
+        });
+
+        const a = new AnchorElement({
+            className   : 'btn btn-primary btn-lg',
+            href        : '#products-container',
+            text        : 'Ver Productos'
         });
 
         const html = `
             ${h1.getHeading().outerHTML}
             ${p.getParagraph().outerHTML}
-            <a href="#products-container" class="btn btn-primary btn-lg">Ver Productos</a>`;
+            ${a.getAnchor().outerHTML}`;
         this.setHTML(html);
     }
 }
