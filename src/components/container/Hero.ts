c522126a449f1463103bd6b0e5492df1c7838eb1
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2025-11-21 23:28:03 -0300

    Se adpta src/components/container/Hero.ts a la nueva clase Paragraph.ts

--- src/components/container/Hero.ts
+++ src/components/container/Hero.ts
@@ -1,4 +1,6 @@
 import { Container } from "../Container.js";
+import { HeadingElement } from "../elements/Heading.js";
+import { ParagraphElement } from "../elements/Paragraph.js";
 
 export class Hero extends Container {
     constructor(id: string, className: string) {
@@ -7,9 +9,20 @@ export class Hero extends Container {
     }
     
     private createHTML (): void {
+        const h1 = new HeadingElement({
+            className: 'display-4 fw-bold mb-4',
+            text: 'Calzados para Toda la Familia',
+            type: 'h1'
+        });
+
+        const p = new ParagraphElement({
+            className: 'lead mb-4 fw-semibold',
+            text: 'Somos distribuidores de una gran variedad de modelos de calzado infantil y calzado para adultos.'
+        });
+
         const html = `
-            <h1 class="display-4 fw-bold mb-4">Calzados para Toda la Familia</h1>
-            <p class="lead mb-4 fw-semibold">Somos distribuidores de una gran variedad de modelos de calzado infantil y calzado para adultos.</p>
+            ${h1.getHeading().outerHTML}
+            ${p.getParagraph().outerHTML}
             <a href="#products-container" class="btn btn-primary btn-lg">Ver Productos</a>`;
         this.setHTML(html);
     }
